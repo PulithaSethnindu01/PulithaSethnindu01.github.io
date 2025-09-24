b772ec2c75beb9ff015a16c4a0716d288243d91a
Pulitha Sethnindu <96250488+PulithaSethnindu01@users.noreply.github.com>
2025-09-24 22:29:07 +0530

    Add files via upload

---
+++ src/app/layout.tsx
@@ -0,0 +1,70 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Manrope } from 'next/font/google'
+import { cn } from '@/lib/utils'
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const fontHeading = Manrope({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-heading',
+})
+
+const fontBody = Manrope({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-body',
+})
+
+export const metadata: Metadata = {
+  title: "SACCMCT '24",
+  description: "Sir Arthur C. Clarke Memorial Challenge Trophy 2024 - Anandian Astronomical Association",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+
+
+
+// import { Manrope } from 'next/font/google'
+// import { cn } from '@/lib/utils'
+// import './globals.css'
+
+// const fontHeading = Manrope({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-heading',
+// })
+
+// const fontBody = Manrope({
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-body',
+// })
+
+// export default function Layout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body 
+//         className={cn(
+//           'antialiased',
+//           fontHeading.variable,
+//           fontBody.variable
+//         )}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   )
+// }
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { Component } from "@/components/component/component";
+import './globals.css'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-2">
+      <Component />
+      
+    </main>
+  );
+}
